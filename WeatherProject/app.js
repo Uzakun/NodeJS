0e588bfd8a666bc37ce79e2169883f6e52e31bd8
Shwetank Chandra <96305925+Uzakun@users.noreply.github.com>
2023-06-30 21:04:46 +0530

    Comlete weatherProject

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -12,7 +12,13 @@ app.get("/", function(req, res){
     response.on("data", function(data){
         const weatherData = JSON.parse(data);
         const temp = weatherData.main.temp;
-        const description = weatherData.weather[0].description;
+        const weatherDescription = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        res.write("<p>The weather is currently " + weatherDescription + ".<p>");
+        res.write("<h1>The temperature in India is "+ temp + "degree Celcius</h1>");
+        res.write("<img src="+ imageURL +">");
+        res.send();
     });    
 
   });
